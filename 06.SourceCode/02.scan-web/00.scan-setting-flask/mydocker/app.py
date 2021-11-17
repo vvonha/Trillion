f647e4f8503ad04d2d66374e239b52bbdcaac874
soonang <nr97819@naver.com>
2021-11-17 22:18:34 +0900

    02.scan-web 테스트중(에러)

--- 06.SourceCode/02.scan-web/00.scan-setting-flask/mydocker/app.py
+++ 06.SourceCode/02.scan-web/00.scan-setting-flask/mydocker/app.py
@@ -3,21 +3,24 @@ import pytesseract
 from pytesseract import *
 import numpy as np
 
-img = cv2.imread('./test.png')
+def texting(file_and_path):
+    img = cv2.imread(file_and_path)
 
-# 회색 변환(channel 수 감소)
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 회색 변환(channel 수 감소)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 이진수 변환(흑 / 백)
-img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    # 이진수 변환(흑 / 백)
+    img_binary = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-# 모폴로지 연산(Opening)
-kernel = np.ones((5, 5), np.uint8)
-img_open = cv2.morphologyEx(img_binary, cv2.MORPH_OPEN, kernel)
+    # 모폴로지 연산(Opening)
+    kernel = np.ones((5, 5), np.uint8)
+    img_open = cv2.morphologyEx(img_binary, cv2.MORPH_OPEN, kernel)
 
-# tesseract 설정
-custom_config = '--oem 3' # LSTM 버전
-text = pytesseract.image_to_string(img_binary, 'eng+Hangul') # 한글은 kor이 아닌 Hangul
+    # tesseract 설정
+    custom_config = '--oem 3' # LSTM 버전
+    text = pytesseract.image_to_string(img_binary, 'eng+Hangul') # 한글은 kor이 아닌 Hangul
 
-with open('file.txt', mode='w') as f:
-    f.write(text)
+    with open('file.txt', mode='w') as f:
+        f.write(text)
+        
+    return text
---
+++ 06.SourceCode/02.scan-web/00.scan-setting-flask/mydocker/main.py
@@ -0,0 +1,46 @@
+import re
+import os
+from flask import Flask, jsonify, request, render_template, make_response, session, redirect, url_for
+from app import texting
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def home():
+    return render_template('scan.html')
+
+@app.route('/scan', methods=['GET','POST'])
+def scan():
+    print('# 스캔 로직 작동중...')
+    
+    file = request.files['file']
+    file_and_path = os.path.join('./', file.filename)
+    file.save(file_and_path)
+    print('# 업로드 완료...')
+    
+    # opened_file = open(file_and_path, 'r', encoding='UTF8')
+    # text=''.join(opened_file.readlines())
+    # text=opened_file.readlines()
+    # print(text)
+    temp = texting(file_and_path)
+    print(temp)
+    print('# 텍스팅 처리 완료...')
+    
+    return redirect('/')
+
+    # if "Patient's Name:" in text:
+    #     print('Exist!')
+    
+    # Patient=examine(text, "Patient's Name:", 100, "Date of birth:")
+    # birth=examine(text, "Date of birth:", 100, "Ward:")
+    # Ward=examine(text, "Ward:", 100, "Hospital:")
+    # Hospital=examine(text, "Hospital:", 100, "Consultant:")
+    # Consultant=examine(text, "Consultant:", 100, "\n")
+    
+    
+    # result(검증 문자열) 추가된 것 확인
+    # return render_template('/register/reg_step_3_valid.html', username=username, password=password, 
+    #                        Patient=Patient, birth=birth, Ward=Ward, Hospital=Hospital, Consultant=Consultant)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
