c0ca7c1017472b236cc74f38356b7ad85bea2229
soonang <nr97819@naver.com>
2021-11-26 09:16:05 +0900

    rds 도메인 수정

--- 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_model/mysql.py
+++ 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_model/mysql.py
@@ -1,14 +1,15 @@
 import pymysql
 
 # user_info_table
-# MYSQL_HOST = 'database-private.cpoaafcf6ree.ap-northeast-2.rds.amazonaws.com'
-MYSQL_HOST = 'database-1.cpoaafcf6ree.ap-northeast-2.rds.amazonaws.com'
+# MYSQL_HOST = 'database-1.cpoaafcf6ree.ap-northeast-2.rds.amazonaws.com'
+MYSQL_HOST = 'healers-db-instance-1.cp54nqphq6gx.ap-northeast-2.rds.amazonaws.com'
 MYSQL_CONN = pymysql.connect(
     host=MYSQL_HOST,
     port=3306,
-    user='admin',
-    # passwd='tkrwp911',
-    passwd='Password!',
+    # user='admin',
+    user='healers',
+    # passwd='Password!',
+    passwd='Trillion!',
     db='user_data_schema',
     charset='utf8'
 )
