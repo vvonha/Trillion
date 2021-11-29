571df009b81bb1c1c8aea48e66111966e4548a02
soonang <nr97819@naver.com>
2021-11-29 14:24:05 +0900

    3_valid 기존거 임시 활성화

--- 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_view/view.py
+++ 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_view/view.py
@@ -140,50 +140,50 @@ def examine(text, str, start_idx, end_str):
             return(examined)
             # print(text[i+len("Patient's Name: "):text.find(' ',i)])
             
-# @acc_obj.route('/register_3_validation', methods=['GET','POST']) # 접속할 URL
-# def register_level_3_temp():
-#     print('level = 3_temp')
-#     print('username : ', request.form['username'])
-#     print('password : ', request.form['password'])
-#     username=request.form['username']
-#     password=request.form['password']
+@acc_obj.route('/register_3_validation', methods=['GET','POST']) # 접속할 URL
+def register_level_3_temp():
+    print('level = 3_temp')
+    print('username : ', request.form['username'])
+    print('password : ', request.form['password'])
+    username=request.form['username']
+    password=request.form['password']
     
-#     # Test Code
-#     file = request.files['file']
-#     # file_and_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-#     file_and_path = os.path.join('./', file.filename)
-#     file.save(file_and_path)
-#     #return 'file uploaded successfully'
+    # Test Code
+    file = request.files['file']
+    # file_and_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file_and_path = os.path.join('./', file.filename)
+    file.save(file_and_path)
+    #return 'file uploaded successfully'
     
-#     opened_file = open(file_and_path, 'r', encoding='UTF8')
-#     text=''.join(opened_file.readlines())
-#     # text=opened_file.readlines()
-#     # print(text)
+    opened_file = open(file_and_path, 'r', encoding='UTF8')
+    text=''.join(opened_file.readlines())
+    # text=opened_file.readlines()
+    # print(text)
     
-#     if "Patient's Name:" in text:
-#         print('Exist!')
+    if "Patient's Name:" in text:
+        print('Exist!')
         
-#     # start=80
-#     # while("Patient's Name:" in text):
-#     #     i=text.find('P',start) # 100
-#     #     if i==-1:
-#     #         break
-#     #     start=i+1
-#     #     print('i:',i)
-#     #     print(text[i:i+16])
-#     #     # if text[i:i+15]=="Patient's Name: ":
-#     #     #     print(text[i+16:i+30])
+    # start=80
+    # while("Patient's Name:" in text):
+    #     i=text.find('P',start) # 100
+    #     if i==-1:
+    #         break
+    #     start=i+1
+    #     print('i:',i)
+    #     print(text[i:i+16])
+    #     # if text[i:i+15]=="Patient's Name: ":
+    #     #     print(text[i+16:i+30])
     
-#     Patient=examine(text, "Patient's Name:", 100, "Date of birth:")
-#     birth=examine(text, "Date of birth:", 100, "Ward:")
-#     Ward=examine(text, "Ward:", 100, "Hospital:")
-#     Hospital=examine(text, "Hospital:", 100, "Consultant:")
-#     Consultant=examine(text, "Consultant:", 100, "\n")
+    Patient=examine(text, "Patient's Name:", 100, "Date of birth:")
+    birth=examine(text, "Date of birth:", 100, "Ward:")
+    Ward=examine(text, "Ward:", 100, "Hospital:")
+    Hospital=examine(text, "Hospital:", 100, "Consultant:")
+    Consultant=examine(text, "Consultant:", 100, "\n")
     
     
-#     # result(검증 문자열) 추가된 것 확인
-#     return render_template('/register/reg_step_3_valid.html', username=username, password=password, 
-#                            Patient=Patient, birth=birth, Ward=Ward, Hospital=Hospital, Consultant=Consultant)
+    # result(검증 문자열) 추가된 것 확인
+    return render_template('/register/reg_step_3_valid.html', username=username, password=password, 
+                           Patient=Patient, birth=birth, Ward=Ward, Hospital=Hospital, Consultant=Consultant)
 
 @acc_obj.route('/register_3_validation_2', methods=['GET','POST']) # 접속할 URL
 def register_level_3_temp_2():
