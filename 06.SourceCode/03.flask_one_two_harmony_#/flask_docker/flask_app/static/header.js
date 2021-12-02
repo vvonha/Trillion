638919207202cfdb2a32fcfd069bb650262c7fbd
soonang <nr97819@naver.com>
2021-12-02 15:19:49 +0900

    링크 및 로그인 db 버그 수정

---
+++ 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/static/header.js
@@ -0,0 +1,9 @@
+document.write('\
+<div class="py-5 text-center">\
+  <a href="/service/home">\
+    <img class="d-block mx-auto mb-4" src="/static/images/skhealers_logo.png" alt="" width="250" height="113">\
+  </a>\
+  <h4>회원가입 페이지</h4>\
+  <p class="lead">회원가입에 필요한 정보들을 작성해주세요.</p>\
+</div>\
+')
