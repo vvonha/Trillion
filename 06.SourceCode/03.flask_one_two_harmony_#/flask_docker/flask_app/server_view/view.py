0a1cf7d228a64224b7d45721099ea9657ceb0678
soonang <nr97819@naver.com>
2021-12-02 18:18:25 +0900

    test

--- 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_view/view.py
+++ 06.SourceCode/03.flask_one_two_harmony_#/flask_docker/flask_app/server_view/view.py
@@ -213,7 +213,7 @@ def register_level_3_temp():
     if (ext != '.png') and (ext != '.txt') and (ext != '.docx'):
         print('@#$지원하지 않는 파일 형식 발견!')
         # return render_template('/register/reg_step_3.html', warning='지원되지 않는 파일 형식입니다.', username=username, password=password)
-        return redirect(url_for('account.register_level_3', warning='지원되지 않는 파일 형식입니다.', username=username, password=password))
+        return redirect('/')
     
     # file_and_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
     file_and_path = os.path.join('./', file.filename)
